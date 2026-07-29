493fc3ad20f712c2f726fd4a9f3ee04a38ea1ba7
SandeepDayanidhi <sandeepd.27qe@gmail.com>
2026-07-29 22:24:04 +0530

    testwith commands

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("hello")
+
+git init
+git status
+git add hello.py
+git branch -M main
+git remote add origin https://github.com/SandeepDayanidhi/AILearningPhase1
+git config --global user.email "sandeepd.27qe@gmail.com"
+git config --global user.name "SandeepDayanidhi"
+git config --global user.email
+git commit -m "test"
+git push -u origin main
